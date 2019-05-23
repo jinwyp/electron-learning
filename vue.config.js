634726c5f23736f478a9291cc58227601f103291
wangyupeng <wangyp@zenmen.com>
2019-05-23 17:29:43 +0800

    U-update

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,6 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             builderOptions: {
-                chainWebpackMainProcess: config => {
-                    config.target('electron-renderer')
-                    // Chain webpack config for electron main process only
-                },
-                chainWebpackRendererProcess: config => {
-                    // Chain webpack config for electron renderer process only
-                    // The following example will set IS_ELECTRON to true in your app
-                    config.target('electron-renderer')
-                },
-                
                 extraFiles: [
                     {
                         "from": "resources/${os}",
@@ -21,7 +11,19 @@ module.exports = {
                         "filter": ["**/*"]
                     }
                 ]
-            }
+            },
+
+            chainWebpackMainProcess: config => {
+                config.target('electron-renderer')
+                // Chain webpack config for electron main process only
+            },
+            chainWebpackRendererProcess: config => {
+                // Chain webpack config for electron renderer process only
+                // The following example will set IS_ELECTRON to true in your app
+                config.target('electron-renderer')
+            },
+
+            
         }
     }
 }
